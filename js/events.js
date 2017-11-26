53ddbbd39e204e76ad2a7751182986a8054d9416
Devon Langendoerfer <425devon@gmail.com>
2017-11-26 08:19:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,14 +8,14 @@ function frameIt(){
     $("img").addClass("tasty")
   });
 }
-/*
+
 function pressIt(){
-  $(document).on('keydown', function(key) {
+  $("input").on('keydown', function(key) {
     if(key.which == 71){
         alert('g was pressed');
 }
 });
-*/
+
 
 $(document).ready(function(){
 
