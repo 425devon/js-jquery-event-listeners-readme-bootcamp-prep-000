e2937398fdc4754b607d2f26573582a54ae0f27d
Devon Langendoerfer <425devon@gmail.com>
2017-11-26 08:08:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,12 @@ function getIt(){
   $('p').on("click",function(){alert("Hey!")})
 }
 
+function frameIt(){
+  $('img').on('load', function(){
+    this.addClass('tasty')
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
